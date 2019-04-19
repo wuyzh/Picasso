dfd8a935459d15c7aaf765b647a010b8d81a762f
wuyazhou <wuyazhou-ds@gome.com.cn>
2019-04-19 11:24:28 +0800

    网络异常

---
+++ app/src/main/java/com/wuyazhou/learn/picasso/use/CallbackToMainThread.java
@@ -0,0 +1,94 @@
+package com.wuyazhou.learn.picasso.use;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Looper;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * @author wuyzh
+ * @fuction 将返回结果直接回调给UI线程，整个方法都在UI线程内执行
+ * CallbackToMainThread实现自Callback，Callback是OkHttp3请求回调要实现的接口
+ * */
+public class CallbackToMainThread implements Callback {
+    private Callback mCallback = null;
+    private Call mCall = null;
+    private IOException mIOException = null;
+    private String mStringResponse = null;
+    private Bitmap mBitmapResponse = null;
+    /**
+     * @param callback 这里是请求结果的回调，其回调方法将会在UI线程内执行
+     * */
+    public CallbackToMainThread(Callback callback){
+        mCallback = callback;
+    }
+    /**
+     * 此处new android.os.Handler(Looper.getMainLooper()).post(runnableFailureUI)是将失败通过UI线程返回
+     * */
+    @Override
+    public void onFailure(Call call, IOException e) {
+        mCall = call;
+        mIOException = e;
+        new android.os.Handler(Looper.getMainLooper()).post(runnableFailureUI);
+    }
+
+    /**
+     * 这里会根据主线程内Callback的getTClassName实现来获取想要得到的数据类型，然后去做类型转化
+     * */
+    @Override
+    public void onResponse(Call call, Response response) {
+        mCall = call;
+        try {
+            if (mCallback.getTClassName().equals(String.class.getName())){
+                mStringResponse = response.body().string();
+            }else if (mCallback.getTClassName().equals(Bitmap.class.getName())){
+                byte[] data = response.body().bytes();
+                BitmapFactory.Options decodeOptions = new BitmapFactory.Options();
+
+                decodeOptions.inPreferredConfig = Bitmap.Config.RGB_565;
+                mBitmapResponse = BitmapFactory.decodeByteArray(data, 0, data.length, decodeOptions);
+            }
+        } catch (IOException e) {
+            mIOException = e;
+            new android.os.Handler(Looper.getMainLooper()).post(runnableFailureUI);
+        }
+        new android.os.Handler(Looper.getMainLooper()).post(runnableSuccessUI);
+    }
+    Runnable runnableFailureUI = new Runnable() {
+        @Override
+        public void run() {
+            mCallback.onFailure(mCall,mIOException);
+        }
+    };
+
+    Runnable runnableSuccessUI = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                if (mStringResponse != null){
+                    mCallback.onResponse(mCall,mStringResponse);
+                }else if (mBitmapResponse != null){
+                    mCallback.onResponse(mCall,mBitmapResponse);
+                }
+
+            } catch (IOException e) {
+                mCallback.onFailure(mCall,e);
+            }
+        }
+    };
+
+    /**
+     * 此处接口将会在UI线程内实现
+     * 成功失败都将会通过这里面的方法返回给UI线程
+     * */
+    public interface Callback<T> {
+        void onFailure(Call call, IOException e);
+        void onResponse(Call call, T response) throws IOException;
+        String getTClassName();
+    }
+}
--- app/src/main/java/com/wuyazhou/learn/picasso/use/PicassoUsePagerView.java
+++ app/src/main/java/com/wuyazhou/learn/picasso/use/PicassoUsePagerView.java
@@ -1,12 +1,15 @@
 package com.wuyazhou.learn.picasso.use;
 
 import android.content.Context;
+import android.net.Uri;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.squareup.picasso.Picasso;
 import com.wuyazhou.learn.logview.LogShowUtil;
 import com.wuyazhou.learn.picasso.R;
 
@@ -20,12 +23,18 @@ import butterknife.OnClick;
  * @function
  */
 public class PicassoUsePagerView extends FrameLayout {
+    public static final String IMAGE_URL_1 = "https://ss.csdn.net/p?https://mmbiz.qpic.cn/mmbiz_jpg/trm5VMeFp9mJejJH2asZZT0ML63erOW3QAMSkjEMsLwByykbJwsHj7QmPbQDDUU43BJpHTXxyiaY24LXlA6zKDQ/640?wx_fmt=jpeg";
+    public static final String IMAGE_URL_2 = "http://imgup01.myra2.com/2017-04/13/11/1492055267740_0.jpg";
+    public static final String IMAGE_URL_3 = "http://i1.hexun.com/2017-10-20/191306534.jpg";
+    public static final String IMAGE_URL_4 = "http://www.pujia8.com/static/pics/20161116120001_22.jpg";
     private Context mContext = null;
     private RelativeLayout mLayout;
     @BindView(R.id.model_button_1)
     Button button01;
     @BindView(R.id.model_button_2)
     Button button02;
+    @BindView(R.id.image)
+    ImageView mImageView;
 
 
     public PicassoUsePagerView(Context context) {
@@ -57,9 +66,11 @@ public class PicassoUsePagerView extends FrameLayout {
     @OnClick(R.id.model_button_1)
     void onClickButton01(){
         LogShowUtil.addLog("Picasso","点击按钮01");
+        Picasso.with(mContext).load(Uri.parse(IMAGE_URL_1)).placeholder(R.drawable.ic_launcher_background).into(mImageView);
     }
     @OnClick(R.id.model_button_2)
     void onClickButton02(){
         LogShowUtil.addLog("Picasso","点击按钮02");
+        Picasso.with(mContext).load((IMAGE_URL_2)).error(R.drawable.clear).into(mImageView);
     }
 }
